7b80626f46900500a855111a83dabcbc0d2a1221
muhammadBilal924 <mbilal60924@gmail.com>
2022-02-15 19:06:46 +0500

    first commit

---
+++ components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+<dir>
+    <h1>Hello from Home Component</h1>
+     <button> Hy Bilal</button>
+    </dir>
+</template>
+<script>
+export default {
+     name:"Home",
+     props:{
+         data:String,
+         msg:String
+
+     },methods:
+     {
+        hello(data)
+        {
+           alert(data)            
+        }         
+     }
+        
+    
+}
+</script>
+
+
+
